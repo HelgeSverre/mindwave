b574004d59a207a70d76de1eadbb07684099db74
Helge Sverre <helge.sverre@gmail.com>
2023-05-14 10:40:09 +0200

    sketch out stuff

--- src/Brain/Brain.php
+++ src/Brain/Brain.php
@@ -2,6 +2,13 @@
 
 namespace Mindwave\Mindwave\Brain;
 
+use Mindwave\Mindwave\Knowledge\Knowledge;
+
 class Brain
 {
+    public function consume(Knowledge $knowledge): self
+    {
+
+        return $this;
+    }
 }
