98fa1ed44fef9d22c89b27203d594f09933af442
Helge Sverre <helge.sverre@gmail.com>
2023-05-24 23:39:28 +0200

    hmm

--- src/Contracts/Vectorstore.php
+++ src/Contracts/Vectorstore.php
@@ -7,11 +7,13 @@ use Mindwave\Mindwave\Vectorstore\Data\VectorStoreEntry;
 
 interface Vectorstore
 {
+    // todo: do we need this?
     public function fetchById(string $id): ?VectorStoreEntry;
 
     /**
      * @return  VectorStoreEntry[]
      */
+    // todo: do we need this?
     public function fetchByIds(array $ids): array;
 
     public function insertVector(VectorStoreEntry $entry): void;
@@ -36,6 +38,6 @@ interface Vectorstore
      */
     public function similaritySearchByVector(EmbeddingVector $embedding, int $count = 5): array;
 
-    // TODO(14 mai 2023) ~ Helge: Wait with this one
+    // TODO(14 mai 2023) ~ Helge: implement
     // public function maxMarginalRelevanceSearchByVector(EmbeddingVector $embedding, int $k = 4, int $fetch_k = 20, float $lambda_mult = 0.5, array $metadata = []): array;
 }
