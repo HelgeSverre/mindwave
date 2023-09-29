1b488e3fb70548dad191af846e68221d0edc7e52
Helge Sverre <helge.sverre@gmail.com>
2023-09-29 17:54:11 +0200

    cleanup

--- config/mindwave.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-
-    'log_channel' => env('MINDWAVE_LOG_CHANNEL', env('LOG_CHANNEL', 'stack')),
-
-];
--- src/MindwaveServiceProvider.php
+++ src/MindwaveServiceProvider.php
@@ -22,12 +22,6 @@ class MindwaveServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('mindwave')
             ->hasConfigFile([
@@ -38,8 +32,6 @@ class MindwaveServiceProvider extends PackageServiceProvider
             ->hasCommands([
                 ToolMakeCommand::class,
             ]);
-        // ->hasViews()
-        // ->hasMigration('create_mindwave_table')
     }
 
     public function registeringPackage()
